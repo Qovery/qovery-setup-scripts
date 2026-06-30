32042bb212710fdb2b927d99ea43e31c5ed0822f
Melvin Zottola <37779145+mzottola@users.noreply.github.com>
2026-06-30 15:54:05 +0200

    feat(qov-1844) Add gcp kms get iam policy permission (#7)

--- scripts/create_credentials_gcp_wif.sh
+++ scripts/create_credentials_gcp_wif.sh
@@ -634,6 +634,8 @@ permissions=(
     "run.services.listEffectiveTags"
     "run.services.listTagBindings"
     "run.services.update"
+    # When using kms key
+    "cloudkms.cryptoKeys.getIamPolicy"
 )
 
 command_exists() {
